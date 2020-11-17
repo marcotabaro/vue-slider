2d1d7109e2699f73e5119f7277a51b7494e25921
marco <g.marcotaba@gmail.com>
2020-11-17 16:57:42 +0100

    init repo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+Creare con Vue uno slider che permetta di:
+Scorrere le immagini tramite appositi bottoni
+Selezionare una immagine tra quelle disponibili
+Avete liberta' su come realizzare l'interfaccia graficamente, per le funzioni prendete come esempio quanto fatto in classe. Per il momento, usate le immagini in allegato.
+Tips & Tricks:
+In Vue le variabili che contengono lo stato dell'applicazione devono essere inizializzate in un oggetto e assegnato al campo  data durante l'inizializzazione dell'istanza di Vue che controlla la vostra app
+Lo stato di una applicazione e' il luogo concettuale dove vengono salvati i dati utili all'applicazione per "funzionare"
+In Vue le funzioni che cambiano lo stato dell'applicazione devono essere definite in un oggetto e assegnato al campo  methods durante l'inizializzazione dell'istanza di Vue che controlla la vostra app
+La generazione di elementi dinamica, dipendente dallo stato dell'applicazione (inizializzato in data), viene dichiarata lato HTML con le direttive introdotte da Vue (ad es. v-if e v-for)
+Gli eventi associati agli elementi dell'interfaccia vengono catturati lato HTML con le direttive che iniziano con v-on:{nome-evento} o @{nome-evento} e associati ad una delle funzioni (dichiarate in methods) definita durante l'inizializzazione di Vue
+Gli attributi degli elementi che devono cambiare in accordo con lo stato dell'applicazione (inizializzato in data) devono essere dichiarati lato HTML con la direttiva v-bind:{nome-attributo} o :{nome-attributo}
+*/
